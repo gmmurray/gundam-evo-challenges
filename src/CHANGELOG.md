5ebba8045af01b56b18d50a8cec5341d66831aa8
gmmurray <trinilime123@live.com>
2023-03-18 23:27:04 -0400

    challenge increment ux improvements

--- src/CHANGELOG.md
+++ src/CHANGELOG.md
@@ -1,23 +1,22 @@
-## [2.1.2] - 2023-03-18
-
+## [2.2.0] - 2023-03-18
+### Added
+- Unit recommendations based off how many challenges each unit appears in
+- Increment damage value by 5,000 instead of 1,000 when total damage goal is 100,000
 ### Fixed
+- Sometimes challenges wouldn't save properly after selecting type, total, and units
 
+## [2.1.2] - 2023-03-18
+### Fixed
 - Clicking "Changes" when notified of a new app version works as expected
 
 ## [2.1.1] - 2023-03-18
-
 ### Added
-
 - Added Sazabi, Asshimar, Dom Trooper, Heavyarms unit grouping
 
 ## [2.1.0] - 2023-03-18
-
 ### Changed
-
 - Selecting challenge type, total, and units automatically adds the challenge to your list (previously you would have to click a button to save the challenge)
 
 ## [2.0.0] - 2023-03-18
-
 ### Added
-
 - This changelog, which can be accessed via the new update notification and in the app page footer
--- src/data/challengeTypes.ts
+++ src/data/challengeTypes.ts
@@ -1,49 +1,53 @@
 import { ChallengeType } from '../types/challenges';
 
-const defaultIncrement: ChallengeType['increment'] = (prev, total) =>
-  Math.min(prev + 1, total);
-const damageIncrement: ChallengeType['increment'] = (prev, total) =>
-  Math.min(prev + 1000, total);
+const incrementer = (prev: number, total: number, amount: number) =>
+  Math.min(prev + amount, total);
+const incrementByOne: ChallengeType['increment'] = (prev, total) =>
+  incrementer(prev, total, 1);
+const incrementByThousands: ChallengeType['increment'] = (prev, total) => {
+  const incrementAmount = total === 100000 ? 5000 : 1000;
+  return incrementer(prev, total, incrementAmount);
+};
 
 export const challengeTypes: Record<string, ChallengeType> = {
   wins: {
     title: 'Win matches',
-    increment: defaultIncrement,
+    increment: incrementByOne,
     options: [1, 7],
   },
   damage: {
     title: 'Inflict damage',
-    increment: damageIncrement,
+    increment: incrementByThousands,
     options: [20000, 100000],
   },
   revives: {
     title: 'Revive teammates',
-    increment: defaultIncrement,
+    increment: incrementByOne,
     options: [1, 5],
   },
   games: {
     title: 'Play matches',
-    increment: defaultIncrement,
+    increment: incrementByOne,
     options: [2, 15],
   },
   crits: {
     title: 'Get critical hit kills',
-    increment: defaultIncrement,
+    increment: incrementByOne,
     options: [1, 5],
   },
   repair: {
     title: 'Repair HP for self or team members',
-    increment: damageIncrement,
+    increment: incrementByThousands,
     options: [2500, 10000],
   },
   kills: {
     title: 'Kills',
-    increment: defaultIncrement,
+    increment: incrementByOne,
     options: [5, 30],
   },
   gman: {
     title: 'Use G-Maneuver',
-    increment: defaultIncrement,
+    increment: incrementByOne,
     options: [3, 20],
   },
 };
